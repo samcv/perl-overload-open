96c44b6422909275d6547f09c0d0a7dd99d87f3d
Samantha McVey <samantha.mcvey@booking.com>
2019-12-18 16:17:40 +0100

    Some more changes to prepare.pl
    Eventually I need to figure out how to release properly

--- prepare.sh
+++ prepare.sh
@@ -9,15 +9,20 @@ fi
 if [ -f Makefile.old ]; then
     rm Makefile.old
 fi
-yml_version=$(grep '^version:' MYMETA.yml | sed 's/version: v//')
-perl_version=$(perl -Ilib -Moverload::open -E 'say $overload::open::VERSION')
+echo $perl_version;
 perl Makefile.PL
 make
+if [ ! -f MYMETA.yml ]; then
+    exit 3
+fi
+yml_version=$(grep '^version:' MYMETA.yml | sed 's/version: v//')
+perl_version=$(perl -Ilib -Moverload::open -E 'say $overload::open::VERSION')
 if [ "$yml_version" != "$perl_version" ]; then
     echo mismatching versions "yml: $yml_version other: $perl_version"
     exit 1
 fi
 meta_version=$(cat MYMETA.json| jq '.version' | sed 's/"//g' | sed 's/^v//')
-if [ "$meta_version" != "$perl_version" ]; then
-    echo "mismatching versions"
+if [ "$meta_version" != "$yml_version" ]; then
+    echo "mismatching versions meto $meta_version and $yml_version"
+    exit 1
 fi
