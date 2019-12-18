5ce7b1e087ba1634ca90c0177de01195f12d4f6c
Samantha McVey <samantha.mcvey@booking.com>
2019-12-18 15:28:28 +0100

    update some stuff

--- prepare.sh
+++ prepare.sh
@@ -9,3 +9,15 @@ fi
 if [ -f Makefile.old ]; then
     rm Makefile.old
 fi
+yml_version=$(grep '^version:' MYMETA.yml | sed 's/version: v//')
+perl_version=$(perl -Ilib -Moverload::open -E 'say $overload::open::VERSION')
+perl Makefile.PL
+make
+if [ "$yml_version" != "$perl_version" ]; then
+    echo mismatching versions "yml: $yml_version other: $perl_version"
+    exit 1
+fi
+meta_version=$(cat MYMETA.json| jq '.version' | sed 's/"//g' | sed 's/^v//')
+if [ "$meta_version" != "$perl_version" ]; then
+    echo "mismatching versions"
+fi
