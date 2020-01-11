8793c67836a3fd4ae0815fd59c86d247c4c48bcd
Samantha McVey <samantham@posteo.net>
2020-01-11 13:22:25 +0100

    Make some fixes to my prepare.sh script

--- prepare.sh
+++ prepare.sh
@@ -7,8 +7,8 @@ if [ -f Makefile ]; then
     make clean
 fi
 perl -MPod::Checker -E 'use strict; use warnings; exit podchecker($ARGV[0], *STDOUT)' lib/overload/open.pm && perldoc -u lib/overload/open.pm > README.pod || exit 1
-if [ -f Manifest ]; then
-    rm Manifest
+if [ -f MANIFEST ]; then
+    rm MANIFEST
 fi
 if [ -f Makefile.old ]; then
     rm Makefile.old
@@ -20,7 +20,7 @@ if [ ! -f MYMETA.yml ]; then
     fail
 fi
 yml_version=$(grep '^version:' MYMETA.yml | sed 's/version: v//')
-perl_version=$(perl -Iblib -Moverload::open -E 'say $overload::open::VERSION')
+perl_version=$(perl -Mblib -Moverload::open -E 'say $overload::open::VERSION')
 if [ "$yml_version" != "$perl_version" ]; then
     echo mismatching versions "yml: $yml_version other: $perl_version"
     fail
